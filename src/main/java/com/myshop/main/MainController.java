6c15877f7857b70cc34e8a5176404014b9c1c1c5
mycom <superant00@naver.com>
2020-01-15 01:49:30 +0900

    bookShop1 ver1.4

--- src/main/java/com/myshop/main/MainController.java
+++ src/main/java/com/myshop/main/MainController.java
@@ -3,20 +3,39 @@ package com.myshop.main;
 
 import com.myshop.common.base.BaseController;
 import com.myshop.goods.service.GoodsService;
+import com.myshop.goods.vo.GoodsVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller("mainController")
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+import java.util.Map;
+
+@Controller
 @EnableAspectJAutoProxy
 public class MainController extends BaseController {
-
     @Autowired
     private GoodsService goodsService;
 
-    @RequestMapping(value = "/")
-    public String main(){
+    @RequestMapping(value= "/main/main.do" ,method={RequestMethod.POST,RequestMethod.GET})
+    public String main(HttpServletRequest request, HttpServletResponse response) throws Exception{
+        System.out.println("===>Controller로 main 메소드 접속");
+        HttpSession session;
+        ModelAndView mav=new ModelAndView();
+        String viewName=(String)request.getAttribute("viewName");
+        mav.setViewName(viewName);
+
+        session=request.getSession();
+        session.setAttribute("side_menu", "user");
+        Map<String, List<GoodsVO>> goodsMap=goodsService.listGoods();
+        mav.addObject("goodsMap", goodsMap);
         return "/main/main";
     }
 }
+
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: mw
+  Date: 2020-01-15
+  Time: 오전 1:27
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<a href="/main/main.do">메인으로 이동</a>
+</body>
+</html>
